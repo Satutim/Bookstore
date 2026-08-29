4ee3e1024411105e5809d1e4ca425cfcd3c2dd72
Satu Timonen <satu.timonen@myy.haaga-helia.fi>
2026-08-29 22:46:30 +0300

    Save Bookstore to Github

---
+++ bookstore/src/main/java/harjoitustyo/bookstore/web/BookController.java
@@ -0,0 +1,14 @@
+package harjoitustyo.bookstore.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class BookController {
+    @GetMapping("index")
+    @ResponseBody
+    public String index() {
+        return "This is Bookstrore";
+    }
+}
